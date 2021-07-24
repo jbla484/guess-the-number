47688643e9202f78068f82e6d9e9ae9e7c3acaf3
James Blankenship <77518971+jbla484@users.noreply.github.com>
2021-07-24 15:08:29 -0700

    Update and rename GuessTheNumber to GuessTheNumber.cpp

---
+++ GuessTheNumber.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+void calculate(int number) {
+    //Finfing a random number between 1-10
+    int randomNumber;
+    randomNumber = rand() % 10 + 1;
+  
+    if (randomNumber == number) {
+        cout << "Congrats, you guessed the right number!" << endl;
+    }
+    else {
+        cout << "Nice try!" << endl;
+    }
+}
+
+int main()
+{
+    while (true) {
+        //Seeding rand for a true random number
+        srand(time(NULL));
+      
+        float number;
+        char cont;
+      
+        cout << "Guess a number between 1-10: ";
+        cin >> number;
+        //Calling calculate function to see if the numbers match
+        calculate(number);
+        
+        cout << "Continue? (y / n): ";
+        cin >> cont;
+        
+        if (cont == 'n') {
+            break;
+        }
+    }
+}
